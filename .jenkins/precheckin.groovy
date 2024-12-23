4d9af0b7bb1ddaa94a151e7d9922bb4aef1373b0
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-12-23 13:35:44 -0700

    Append to LD_LIBRARY_PATH in compile for sles/rhel (#144)
    * Append to LD_LIBRARY_PATH in compile for sles/rhel
    
    * Remove unused configs in precheckin
    
    * Install pybind with --break-system-packages
    
    * Set precheckin ci settings
    
    * Update pip

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -45,10 +45,10 @@ ci: {
     String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
 
     def propertyList = ["compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])]]
-    propertyList = auxiliary.appendPropertyList(propertyList)
+    auxiliary.appendPropertyList(propertyList)
 
-    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu20:['gfx90a'], ubuntu22:['gfx1101'], sles15sp1:['gfx908'], rhel8:['gfx1030'], rhel9:['gfx942']])]
-    jobNameList = auxiliary.appendJobNameList(jobNameList)
+    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([rhel9:['gfx1101'], sles15sp1:['gfx908'], ubuntu22:['gfx942'], ubuntu24:['gfx90a']])]
+    auxiliary.appendJobNameList(jobNameList)
 
     propertyList.each 
     {
@@ -68,7 +68,6 @@ ci: {
         }
     }
 
-    // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
     if(!jobNameList.keySet().contains(urlJobName)) {
         properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
         stage(urlJobName) {
